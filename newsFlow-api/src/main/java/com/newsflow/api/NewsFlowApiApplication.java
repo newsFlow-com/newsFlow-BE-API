225be343d349b8464a4ad11606c8e120098df642
CoderGogh <haram1021@gmail.com>
2026-05-13 16:32:10 +0900

    chore: initialize Spring Boot project

---
+++ newsFlow-api/src/main/java/com/newsflow/api/NewsFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.newsflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFlowApiApplication.class, args);
+    }
+
+}
---
+++ newsFlow-api/src/test/java/com/newsflow/api/NewsFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
